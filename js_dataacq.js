ec79c6fc516b6fc7f81b7ac8a79cfecc4f08fe47
JTMidel <100242239+JTMidel1@users.noreply.github.com>
2022-06-24 09:42:40 +0100

    Add files via upload

---
+++ js_dataacq.js
@@ -0,0 +1,7 @@
+const Username = 'Bamidele Emmanuel'
+const UserHeight = '1.500 Meters'
+const UserCountry = 'Nigeria'
+
+alert('My name is: ' + Username)
+alert('My height is: ' + UserHeight)
+alert("I'm from " + UserCountry)
